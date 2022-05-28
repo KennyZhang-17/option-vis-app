d331774044e369215ff748266af6f02ee779d274
KennyZhang-17 <35554830+KennyZhang-17@users.noreply.github.com>
2022-05-27 20:14:59 -0700

    Test selector

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ from dash.dependencies import Input, Output
 import os
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+from datetime import datetime
+import numpy as np
+import json
+import panel as pn
+pn.extension('vega')
 alt.data_transformers.disable_max_rows()
 
 import datetime as dt
@@ -19,7 +25,7 @@ path=os.path.join(os.path.dirname(__file__), '{}/{}{}{}'.format(name, name, date
     #df = pd.read_csv('os.path.dirname(__file__)/{}/{}{}{}'.format(name, name, date,'close'))
 df=pd.read_csv(path)
 
-fig = px.scatter(df, x="gamma", y="delta")
+#fig = px.scatter(df, x="gamma", y="delta")
 
 def plot_altair(date=dt.date(2022,4,21),name="SPY"):
     path=os.path.join(os.path.dirname(__file__), '{}/{}{}{}'.format(name, name, date,'close'))
@@ -134,16 +140,68 @@ def plot_altair(date=dt.date(2022,4,21),name="SPY"):
         ).add_selection(optionSelect)
     return Selection_strikeAndExpiration(Selection_putCall(markPrice)).to_html()
 
+
+name = 'TSLA'
+
+df = pd.read_csv('./{}/{}'.format(name,name+'_PriceHistory'))
+#df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+df['datetime']=[datetime.fromtimestamp(i/1000) for i in df['datetime']]
+fig = go.Figure(data=[go.Candlestick(x=df['datetime'],
+                open=df['open'],
+                high=df['high'],
+                low=df['low'],
+                close=df['close'])])
+fig=fig.update_layout(xaxis_rangeslider_visible=False)
+range=list(df['datetime'].values)+[max(df['datetime'])+dt.timedelta(days=i+1) for i in range(30)]
+fig = fig.add_traces(px.scatter(
+        x=np.repeat(range,500), y=np.tile(np.linspace(np.min(df["low"])-np.std(df["low"]),
+        np.max(df['high'])+np.std(df["high"]), len(range)), 500)
+    )
+    .update_traces(marker_color="rgba(0,0,0,0)")
+    .data
+)
+name = 'TSLA'
+# date = dt.date.today()
+date = dt.date(2022,5,1)
+df=pd.read_csv('./{}/{}{}{}'.format(name, name, date,'close'))
+# Option Selector
+chart = alt.Chart(df)#.encode(color=putCall_color)
+putCall_color = alt.Color('putCall:N',legend=None,scale=alt.Scale(domain=['PUT','CALL'],range=['red','green']))
+width = 500
+
+buy_selector = alt.selection_multi(name="buy")
+
+buycolor = alt.condition(buy_selector,
+                putCall_color,
+                alt.value('lightgray'))
+buy = chart.mark_point(size=150).encode(
+    alt.X('strikePrice:Q'),
+    alt.Y('putCall:N'),
+    color = buycolor
+    ).add_selection(buy_selector).interactive().properties(width=width)
+vega_pane = pn.pane.Vega(buy, debounce=10)
+
+
+def expiration_price(name="TSLA",callput="PUT"):
+    date = dt.date(2022,5,1)
+    df = pd.read_csv('./{}/{}{}{}'.format(name, name, date,'close'))
+    df = df[df["putCall"]==callput]
+    df['expirationDate']=[datetime.fromtimestamp(i/1000) for i in df['expirationDate']]
+    df = df[df["expirationDate"].dt.date<dt.date(2023,1,1)]
+    fig = px.scatter(df, x="strikePrice", y="expirationDate")
+    return fig
+
 app = dash.Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 server = app.server
 
 
 app.layout = html.Div([
-        # dcc.Dropdown(
-        #     id='bs', value='Buy',
-        #     options=[
-        #     {'label': 'Buy', 'value': 'Buy'},
-        #     {'label': 'Sell', 'value': 'Sell'}]),
+        #html.P(str(vega_pane.selection.buy)),
+        dcc.Dropdown(
+            id='putcall', value='PUT',
+            options=[
+            {'label': 'PUT', 'value': 'PUT'},
+            {'label': 'CALL', 'value': 'CALL'}]),
         dcc.Dropdown(
             id='stock', value='SPY',
             options=[
@@ -155,37 +213,55 @@ app.layout = html.Div([
                     max_date_allowed=date_max,
                     initial_visible_month=dt.date(2022, 4, 1),
                     date=dt.date(2022, 4, 21)),
-        html.Iframe(
-            id='scatter',
-            style={'border-width': '0', 'width': '100%', 'height': '400px'},
-            srcDoc=plot_altair(date=dt.date(2022, 4, 21),name='SPY')),
+        # html.Iframe(
+        #     id='scatter',
+        #     style={'border-width': '0', 'width': '100%', 'height': '400px'},
+        #     srcDoc=plot_altair(date=dt.date(2022, 4, 21),name='SPY')),
+        dcc.Graph(figure=expiration_price(name="TSLA",callput="CALL"),id="expiration_price"),
+        # html.Iframe(
+        #     id='option_selector',
+        #     style={'border-width': '0', 'width': '100%', 'height': '400px'},
+        #     srcDoc=buy.to_html()),
         dcc.Graph(figure=fig,id="plot"),
-        dcc.Textarea(id='widget')])
+        dcc.Textarea(id='widget'),
+        dcc.Textarea(id='widget2'),
+        ])
 
-@app.callback(
-    Output('scatter', 'srcDoc'),
-    Input('date-single', 'date'),
-    Input('stock', 'value')
-    #nput('pc', 'value'),
-    )
+# @app.callback(
+#     Output('scatter', 'srcDoc'),
+#     Input('date-single', 'date'),
+#     Input('stock', 'value')
+#     #nput('pc', 'value'),
+#     )
+#
+# def update_output(date,value):
+#     return plot_altair(date,value)
 
-def update_output(date,value):
-    return plot_altair(date,value)
-    
 @app.callback(
     Output("widget", "value"),
-     [Input("plot", "selectedData"), Input("plot", "clickData")]
+    Input("plot", "clickData")
 )
-def update_widget(value,clickData):
-    holder = []
+def update_widget(clickData):
     if(clickData):
-        holder.append(str(clickData["points"][0]['x'])+","+str(clickData["points"][0]['y']))
-    if(value):
-        for x in value["points"]:
-            holder.append(str(x['x'])+str(x['y']))
-    return str(holder)
+        return json.dumps({k: clickData["points"][0][k] for k in ["x", "y"]})
 
+@app.callback(
+    Output("expiration_price", "figure"),
+    Input('putcall', 'value'),
+    Input('stock', 'value')
+)
 
+def update_plot(a,b):
+    return expiration_price(name=b,callput=a)
+
+@app.callback(
+    Output("widget2", "value"),
+    Input('expiration_price', 'clickData'),
+)
+
+def update_widget2(clickData):
+    if(clickData):
+        return json.dumps({k: clickData["points"][0][k] for k in ["x", "y"]})
 
 
 if __name__ == '__main__':
