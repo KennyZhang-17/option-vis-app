3cbff6c6c94dc8bf1e08be71a388f850e3c2df8a
KennyZhang-17 <35554830+KennyZhang-17@users.noreply.github.com>
2022-06-01 13:24:56 -0700

    Fix website

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import dash
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
-import dash_bootstrap_components as dbc
 import os
 import pandas as pd
 import plotly.express as px
